d6a48230c4bbf2368644f0eef3ced96f4b336f2b
Maron martell Mraz <steklyanov@gmail.com>
2019-08-11 14:47:44 +0300

    add main validation structure

--- inc/lemin.h
+++ inc/lemin.h
@@ -6,7 +6,7 @@
 /*   By: mmraz <mmraz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/06 12:55:54 by mmraz             #+#    #+#             */
-/*   Updated: 2019/08/11 13:58:44 by mmraz            ###   ########.fr       */
+/*   Updated: 2019/08/11 14:39:42 by mmraz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,13 @@
 typedef struct s_farm	t_farm;
 typedef struct s_room	t_room;
 
+/* structure for writing graph edges  */
+typedef struct      s_conn
+{
+	t_room          *room;
+	struct s_conn	*next;
+}                   t_conn;
+
 /* main room structure  */
 struct      s_room
 {
@@ -32,22 +39,18 @@ struct      s_room
 	int     blame;
 	int     res_3;
 	int     res_4;
+    t_conn  *connections;
 };
 
 /* main graph structure */
 struct      s_farm
 {
 	t_room  room;
+    int     rooms_amount;
+    int     aunt_amount;
 	t_farm  *next;
 };
 
-/* structure for writing graph edges  */
-typedef struct      s_conn
-{
-	t_room	        room;
-	struct s_conn	*next;
-}                   t_conn;
-
-
+t_farm		*create_farm(char **argv, t_farm *farm);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,17 +6,58 @@
 /*   By: mmraz <mmraz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/06 12:55:52 by mmraz             #+#    #+#             */
-/*   Updated: 2019/08/09 17:13:46 by mmraz            ###   ########.fr       */
+/*   Updated: 2019/08/11 14:46:55 by mmraz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/lemin.h"
 
-int		main(int argc, char **argv)
+
+void		validate_rooms()
 {
 
+}
+
+int			get_operation_numbr(char *line, int operation)
+{
+
+}
+
+/* takes **argv and turns it into a t_farm, */
+/* in case of a error return 0, succes 1 */
+t_farm		*create_farm(char **argv, t_farm *farm)
+{
+	char	*line;
+	int		operation;
+
+	operation = 0;
+	while(get_next_line(0, &line) > 0)
+	{
+		operation = get_operation_nmbr(line, operation);
+		if (operation == 1)
+			validate_room(line, farm);
+		else if (operation == 2)
+			validate_edges(line, farm);
+		else if (operation == 0)
+			get_ants_amount(line);
+		else
+			break;
+	}
+	operation < 0 ? farm->rooms_amount = 0 :ft_printf("");
+	// TODO all necessary frees, t_farm, line
+	return (farm);
+}
+
+int		main(int argc, char **argv)
+{
+	t_farm	*farm;
 	if (argc > 1)
 	{
+		farm = create_farm(argv, farm);
+		if (farm->rooms_amount > 0)
+			print_farm(farm);
 	}
+	else
+		ft_printf("Fu**ing usage");
 	return (0);
 }
